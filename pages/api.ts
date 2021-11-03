11abec51bf9215b4c634a3e9fb5c66245efb6503
Douglas Scriptore <douglass@rpc.com.br>
2021-11-03 14:36:38 -0300

    first commit

--- pages/api.ts
+++ pages/api.ts
@@ -3,6 +3,11 @@ import axios from "axios";
 const api = axios.create({
   // baseURL: 'http://localhost:3333/'
   baseURL: "https://fofocando.herokuapp.com",
+  withCredentials: true,
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
 });
 
 export default api;
