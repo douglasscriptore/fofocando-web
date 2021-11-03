9cc4e6277c47cca71a33b846b1e6c589785076f2
Douglas Scriptore <douglass@rpc.com.br>
2021-11-03 15:49:02 -0300

    first commit

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
     return [
       {
         // matching all API routes
-        source: "/register/:path*",
+        source: "/register",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
