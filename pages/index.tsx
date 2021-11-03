a807778d07373d749299b3def1bf1bea1dfef724
Douglas Scriptore <douglass@rpc.com.br>
2021-11-03 11:42:32 -0300

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,45 @@
+import Head from "next/head";
+import axios from "axios";
+import { useEffect } from "react";
+
+export default function Home({ ip }) {
+  useEffect(() => {
+    const api = axios.create({ baseURL: "https://fofocando.herokuapp.com/" });
+    api
+      .post("/", { ipAddress: ip })
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
+  }, [ip]);
+
+  const currentURL = "http://fofocando.vercel.com.br";
+  const previewImage = "";
+  const siteName = "Fofocando Maringá";
+  const pageTitle = "Bomba Jornalista de Maringá trai esposa";
+  const description =
+    "Um jornalista de Maringá está enfrentando uma situação complicada em sua vida conjugal....";
+
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta property="og:url" content={currentURL} key="ogurl" />
+        <meta property="og:image" content="/jornalismo.jpeg" key="ogimage" />
+        <meta property="og:site_name" content={siteName} key="ogsitename" />
+        <meta property="og:title" content={pageTitle} key="ogtitle" />
+        <meta property="og:description" content={description} key="ogdesc" />
+      </Head>
+
+      <main>
+        <h1>
+          Welcome to <a href="https://nextjs.org"></a>
+        </h1>
+      </main>
+    </div>
+  );
+}
+
+Home.getInitialProps = async ({ req }) => {
+  const ip = req.headers["x-real-ip"] || req.connection.remoteAddress;
+  return { ip };
+};
